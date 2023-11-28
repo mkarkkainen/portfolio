0d03debd12d4688af3c384fd51ff576707fbb40f
Miikka K <miikkak@tuta.io>
2023-11-28 12:01:42 -0600

    clean up data

--- data/projectsData.js
+++ data/projectsData.js
@@ -2,6 +2,59 @@ module.exports = {
   projectsData: [
     {
       id: 1,
+      title: 'OceanExpress CR',
+      label: 'OceanExpress CR',
+      path: '/oceanexpress',
+      coverimg: '/images/beast.jpg',
+      alt: 'Beast Philanthropy',
+      roles: ['Web Development', 'Web Design'],
+      contribution: [
+        'Requirements gathering',
+        'Ideation',
+        'Prototyping',
+        'Gamification',
+        'Design system',
+        'High-fidelity design',
+        'Responsive design',
+        'UI development',
+      ],
+      tools: ['Figma', 'Astro', 'TailwindCSS'],
+      colors: ['#E1447C', '#0DABD2'],
+      brief:
+        'As a UX designer, I led the development of a gamified donation platform that redefines the way people engage with philanthropy. Our platform provides an immersive and rewarding user experience that empowers individuals to make a real impact and compete with others for a chance to earn trophies and recognition. We designed the platform with the goal of making giving a fun and interactive experience, allowing users to see the tangible results of their contributions and experience the joy of giving.',
+      target:
+        'Our target audience is individuals who are interested in supporting various philanthropic causes and want to see the impact of their donations. The platform is designed to attract users who enjoy gaming and competition and are motivated by receiving recognition for their efforts.',
+      objectives: [
+        'Increase engagement with philanthropic campaigns by providing a fun and interactive user experience.',
+        'Encourage users to donate more frequently and in larger amounts by offering rewards, trophies and recognition for their contributions.',
+        'Foster a sense of community and competition by enabling users to compete against each other and their communities.',
+        'Increase awareness of philanthropic campaigns by making it easy for users to share their donations and impact with their social networks.',
+      ],
+      features: [
+        'User-friendly interface for easy browsing and participation in philanthropic campaigns',
+        'Reward system based on engagement and participation',
+        'Dynamic leaderboards showcase top donors and communities',
+        'Social sharing feature to share achievements, impact, and progress on social media.',
+        "Personalised impact reports for individual users and their community's combined impact.",
+        'Trophy case to display earned trophies and track user progress',
+        'Gamified system that incentivises ongoing engagement and creates a sense of achievement',
+        'Visualisation of tangible impact and collective user contributions',
+        'Highlight individual and collective impact of donations',
+        'Inspire culture of giving and make philanthropy fun for everyone.',
+      ],
+      design:
+        'The design of the platform should be visually engaging and consistent with the branding of the philanthropic campaigns it supports. The platform should be designed with a mobile-first approach to ensure a seamless user experience across all devices.',
+      ux: 'The platform should provide a fun, interactive and rewarding user experience that encourages users to stay engaged and motivated to donate. The user journey should be clear and intuitive, with a straightforward donation process and easy-to-navigate features. The gamification system should be challenging yet achievable, with rewards that incentivize users to engage in philanthropic campaigns.',
+      conclusion:
+        'Our gamified donation platform aims to encourage users to participate in philanthropic campaigns, track their impact, collect trophies, and compete against other individuals and communities. By creating a fun, interactive and rewarding user experience, we hope to increase engagement and awareness of philanthropic causes and encourage ongoing donations.',
+      challenge:
+        'Design and build a gamified donation platform based on the existing one. Section features to be developed in phases and plan a feature runway for long-term development.',
+      description:
+        'Independently conceptualized and executed a comprehensive website redesign for a logistics company, seamlessly integrating an innovative design approach with hands-on coding expertise. Crafted an intuitive user interface and implemented responsive web development, resulting in an enhanced online presence for the company and an improved user experience for visitors.',
+      newKey: 'newValue',
+    },
+    {
+      id: 2,
       title: 'Beast Philanthropy',
       label: 'Beast Philantrophy',
       path: '/beast',
@@ -50,11 +103,11 @@ module.exports = {
       challenge:
         'Design and build a gamified donation platform based on the existing one. Section features to be developed in phases and plan a feature runway for long-term development.',
       description:
-        'The Beast Philanthropy website is a donation platform designed to bring visibility and to gather support for different charitable operations around the world. The platform includes a gamified donation system to encourage user engagement and voluntary work through earning prizes and trophies aswell as leaderboards to introduce competition.',
+        'Led the design for a gamified donation platform, seamlessly integrating creative vision into the project. Collaborated closely with a talented front-end development team, contributing to the implementation process while honing my own front-end development skills. The result is a dynamic and engaging platform that merges innovative design with effective functionality, elevating the user experience and fostering a new dimension to charitable giving.',
       newKey: 'newValue',
     },
     {
-      id: 2,
+      id: 3,
       title: 'TestSmarter',
       label: 'TestSmarter',
       path: '/testsmarter',
@@ -92,11 +145,11 @@ module.exports = {
       challenge:
         'Design and develop a responsive website that showcases the best local food spots in the city.',
       description:
-        'Foodie Finds is a website designed to help foodies discover and explore new places to eat in the city. The website features beautiful imagery, detailed descriptions, and an interactive map to make the process of finding new and exciting places to eat easier and more enjoyable.',
+        'Spearheaded the UX/UI design phase for a Patient Management System, collaborating closely with clients to gather requirements and crafting innovative feature prototypes. Transitioning seamlessly into the development phase, I played a dual role as a front-end developer within a dynamic team, translating design concepts into functional, user-friendly features. This multifaceted involvement ensured a cohesive integration of user experience and front-end development, resulting in an enhanced and intuitive patient management solution.',
       newKey: 'newValue',
     },
     {
-      id: 3,
+      id: 4,
       title: 'Yamuni Tabush Foundation',
       label: 'Yamuni Tabush Foundation',
       path: '/yamunitabush',
@@ -130,19 +183,19 @@ module.exports = {
       challenge:
         'Design and develop a responsive website that showcases the best local food spots in the city.',
       description:
-        'Foodie Finds is a website designed to help foodies discover and explore new places to eat in the city. The website features beautiful imagery, detailed descriptions, and an interactive map to make the process of finding new and exciting places to eat easier and more enjoyable.',
+        "As a web designer, I leveraged my creative expertise to craft a visually compelling and user-centric design for an organization's online presence using Figma. Meticulously conceptualizing each element, I created an aesthetically pleasing and functional layout that aligns seamlessly with the organization's brand identity. The result is a thoughtfully designed digital experience poised to elevate the organization's online representation.",
       newKey: 'newValue',
     },
     {
-      id: 4,
+      id: 5,
       title: 'UnoTown',
       label: 'UnoTown',
       path: '/unotown',
       coverimg: '/images/unotown.jpg',
       alt: 'UnoTown',
-      roles: ['UX/UI Designer', 'Co-Founder'],
+      roles: ['Lead Designer', 'Co-Founder'],
       contribution: ['UI Design', 'Front-end Development'],
-      tools: ['Adobe XD, Bootstrap'],
+      tools: ['Adobe XD, CSS, HTML, JavaScript'],
       colors: ['#FFB900', '#DA0037'],
       brief:
         'As a UI designer and front-end developer, I created a responsive website that showcases the best local food spots in the city. The website features beautiful imagery, detailed descriptions, and an interactive map to help foodies discover and explore new places to eat. The website is designed to be simple, easy-to-use, and visually appealing.',
@@ -168,7 +221,7 @@ module.exports = {
       challenge:
         'Design and develop a responsive website that showcases the best local food spots in the city.',
       description:
-        'Foodie Finds is a website designed to help foodies discover and explore new places to eat in the city. The website features beautiful imagery, detailed descriptions, and an interactive map to make the process of finding new and exciting places to eat easier and more enjoyable.',
+        'Led the design efforts for a startup project focused on revolutionizing the house cleaning service industry. As the lead designer, I conceptualized and crafted a user-centric platform aimed at simplifying the process for consumers to effortlessly order cleaning services at competitive prices. By directly connecting consumers with freelance cleaners, eliminating the need for intermediaries, the platform not only enhances user experience but also disrupts the traditional service model, providing a seamless and cost-effective solution for both consumers and freelancers alike.',
       newKey: 'newValue',
     },
   ],
