7fc70271518d189881fe48403ff38845e3522d34
Miikka K <miikkak@tuta.io>
2023-11-28 11:55:49 -0600

    fix darkmode styling issues

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@
 
 export default function Footer() {
   return (
-    <footer className='text-quaternary pointer-events-none border-t-2 border-gray-200 mt-24 pb-24 mx-auto max-w-2xl text-sm dark:border-gray-700 dark:text-gray-600'>
+    <footer className='pointer-events-none border-t-2  mt-24 pb-24 mx-auto max-w-2xl text-sm border-gray-700 text-gray-600'>
       <span>© {new Date().getFullYear()} - miikka k</span>
     </footer>
   );
